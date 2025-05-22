5d24e728457d76def5858a7ec396f955a33b81eb
Clayton Hughes <clayton@vthera.com>
2025-05-21 18:49:56 -0700

    Remove Newtonsoft.Json dependency

--- src/Serilog.Enrichers.Context/Enrichers/KeyValueEnricher.cs
+++ src/Serilog.Enrichers.Context/Enrichers/KeyValueEnricher.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Newtonsoft.Json;
-
 namespace Serilog.Enrichers
 {
     using System.Collections.Generic;
@@ -32,7 +30,7 @@ namespace Serilog.Enrichers
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
             logEvent.AddPropertyIfAbsent(
-                propertyFactory.CreateProperty(_keyValue.Key, JsonConvert.SerializeObject(_keyValue.Value)));
+                propertyFactory.CreateProperty(_keyValue.Key, _keyValue.Value, destructureObjects: true));
         }
     }
 }
